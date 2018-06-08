8d282f34b818133ef4e8adae73f69d24760b114e
Anton Artemov <toxaart@gmail.com>
2018-06-08 09:38:52 +0200

    Modified test a bit

--- test_source/test_matrix_creation.cc
+++ test_source/test_matrix_creation.cc
@@ -28,17 +28,32 @@ static int test_creation() {
 		throw std::runtime_error("Error: M.empty() gave wrong result.");
 
 	M.resize(4, 4);
-	
+    
+    if(M.get_n_rows() != 4 || M.get_n_cols() != 4 )
+        throw std::runtime_error("Error: M.get_n_rows() or get_n_cols() gave wrong result.");
+    
 	if(M.empty())
 		throw std::runtime_error("Error: M.empty() gave wrong result.");
 
 	M.clear();
 	if(!M.empty())
-		throw std::runtime_error("Error: M.empty() gave wrong result.");
+		throw std::runtime_error("Error: M.empty() or clear() gave wrong result.");
 
-	M.resize(16, 16);
+	M.resize(7, 16);
+    
+    if(M.get_n_rows() != 7 || M.get_n_cols() != 16 )
+        throw std::runtime_error("Error: M.get_n_rows() or get_n_cols() gave wrong result.");
+    
+    
 	if(M.get_frob_squared() != 0)
 		throw std::runtime_error("Error: M.get_frob_squared() != 0 for newly created matrix.");
+
+    M.clear();
+    M.resize(32, 32);
+    
+    if(M.get_n_rows() != 32|| M.get_n_cols() != 32 )
+    throw std::runtime_error("Error: M.get_n_rows() or get_n_cols() gave wrong result.");
+    
 
 	double refValue1 = 7.7;
     double refValue2 = 1.1;
@@ -52,28 +67,37 @@ static int test_creation() {
 		rows  [0] = 0;
 		cols  [0] = 0;
 		values[0] = refValue1;
-		rows  [1] = 1;
-		cols  [1] = 1;
+		rows  [1] = 6;
+		cols  [1] = 7;
 		values[1] = refValue2;
 		M.assign_from_vectors(rows, cols, values);
+        
+        if(!M.children_exist())   
+            throw std::runtime_error("Error: M.chilren_exist() gave wrong result.");
+        
 		double expected_frob_sq = refValue1*refValue1 + refValue2*refValue2;
 		
 		if(fabs(M.get_frob_squared() - expected_frob_sq) > 1e-7)
 			throw std::runtime_error("Error: M.get_frob_squared() gave wrong result.");
+            
+        if(M.get_nnz() != nValues)
+            throw std::runtime_error("Error: M.get_nnz() gave wrong result.");
+            
+       
 	}
 
 	  // Test get_values()
 	{
 		int nValues2 = 3;
-		std::vector<int> rows(nValues2);
-		std::vector<int> cols(nValues2);
+		std::vector<int> rows(nValues2), rows_nnz;
+		std::vector<int> cols(nValues2), cols_nnz;
 		rows  [0] = 0;
 		cols  [0] = 0;
-		rows  [1] = 1;
-		cols  [1] = 1;
+		rows  [1] = 6;
+		cols  [1] = 7;
 		rows  [2] = 3;
 		cols  [2] = 3;
-		std::vector<double> values;
+		std::vector<double> values, values_nnz;
 		
 		M.get_values(rows, cols, values);
 		
@@ -85,45 +109,56 @@ static int test_creation() {
 		
 		if(std::fabs(values[2] - 0) > 1e-7)
 			throw std::runtime_error("Error: wrong result from get_values().");
+            
+        M.get_all_values(rows_nnz, cols_nnz, values_nnz);
+        
+        if(rows_nnz.size() != 2)
+            throw std::runtime_error("Error: wrong result from get_all_values().");
+     
+        assert(rows_nnz[0] == 0 && cols_nnz[0] == 0 && values_nnz[0] == refValue1);
+        assert(rows_nnz[1] == 6 && cols_nnz[1] == 7 && values_nnz[1] == refValue2);
 	}
 	
 
 	size_t size = M.get_size();
-	std::cout << "C size before clearing " << size <<std::endl; 
-	
-/*
-	M.clear();
-	
-	size = M.get_size();
-    std::cout << "C size after clearing " << size <<std::endl; */
+    if(size != 516) // two children each 5*4 + 4*8 + 16*8 = 180, plus 3 higher levels 52 bytes each
+        throw std::runtime_error("Error: wrong result from get_size().");
 	
 	std::vector<char> buf(size);
 	M.write_to_buffer(&buf[0], size);
 	
-	
 	MatrixType B;
 	B.assign_from_buffer(&buf[0], size);
-	
-	std::cout << "|M|^2 = " << M.get_frob_squared() << std::endl;
-	std::cout << "|B|^2 = " << B.get_frob_squared() << std::endl;
+    
+    if(M.get_size() != B.get_size())
+        throw std::runtime_error("Error: wrong result from get_size() after assignment from buffer.");
+        
+    if(M.get_nnz() != B.get_nnz())
+       throw std::runtime_error("Error: wrong result from get_nnz() after assignment from buffer.") ;    
+    
+	if(M.get_frob_squared() != B.get_frob_squared())
+        throw std::runtime_error("Error: wrong result from get_frob_squared() after assignment from buffer.");
 	
 	{
-		int nValues3 = 3;
+		int nValues3 = M.get_n_rows() * M.get_n_cols();
 		std::vector<int> rows(nValues3);
 		std::vector<int> cols(nValues3);
-		rows  [0] = 0;
-		cols  [0] = 0;
-		rows  [1] = 1;
-		cols  [1] = 1;
-		rows  [2] = 3;
-		cols  [2] = 3;
+        int counter = 0;
+        for(int i = 0; i < M.get_n_rows(); ++i){
+            for(int j = 0; j < M.get_n_cols(); ++j){
+                rows[counter] = i;
+                cols[counter] = j;
+                counter += 1;
+            }
+        }
 		std::vector<double> values_M, values_B;
 	
 		M.get_values(rows, cols, values_M);
 		B.get_values(rows, cols, values_B);
 		
 		for(int i = 0; i < nValues3; ++i){
-			std::cout << values_M[i] << " " << values_B[i] << std::endl;
+			if(fabs(values_M[i] - values_B[i]) > 1e-12 )
+               throw std::runtime_error("Error: wrong result from get_values() after assignment from buffer.");
 		}
 	
 	}
